3fa8a373e18d609d5f7f5cfb2db3a4bdc1131144
Andrey Klimin <andrey7klimin@yandex.ru>
2025-08-21 15:27:37 +0500

    новый способ сравнения

--- main.c
+++ main.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
+const double EPS = 1e-6;
+
+int isZero(double number);
+int isZero(double number) {
+    if (number > -EPS && number < EPS) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 int main() {
     printf("Введите коэффициенты уравнения ax^2 + bx + c = 0\na b c:\n");
 
-    float a, b, c;
-    scanf("%f %f %f", &a, &b, &c);
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
     double firstRoot, secondRoot;
 
-    if (a == 0) { // проверяем степень уравнения
-        if (b != 0) { // решаем уравнение вида bx + c = 0
+    if (isZero(a)) { // проверяем степень уравнения
+        if (isZero(b)) { // решаем уравнение вида bx + c = 0
             firstRoot = -c / b;
-            printf("Единственное действительное решение уравнения: %f\n", firstRoot);
+            printf("Единственное действительное решение уравнения: %lf\n", firstRoot);
             return 0;
-        } else if (b == 0 && c == 0) { // уравнение вида 0 * x = 0
+        } else if (isZero(b) && isZero(c)) { // уравнение вида 0 * x = 0
             printf("Решение уравнения - множество действительных чисел\n");
             return 0;
         } else { // уравнение вида c = 0
@@ -27,14 +37,14 @@ int main() {
     if (discriminant < 0) {
         printf("Уравнение не имеет действительных решений\n");
         return 0;
-    } else if (discriminant == 0) {
+    } else if (isZero(discriminant)) {
         firstRoot = -b / (2 * a);
-        printf("Единственное действительное решение уравнения: %f\n", firstRoot);
+        printf("Единственное действительное решение уравнения: %lf\n", firstRoot);
         return 0;
     } else {
         firstRoot = (-b - sqrt(discriminant)) / (2 * a);
         secondRoot = (-b + sqrt(discriminant)) / (2 * a);
-        printf("Действительные решения уравнения:\n%f\n%f", firstRoot, secondRoot);
+        printf("Действительные решения уравнения: \n%.4lf\n%.4lf\n", firstRoot, secondRoot);
         return 0;
     }
 }
