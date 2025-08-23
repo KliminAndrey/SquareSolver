96ceea80cd76f6d506040445ece76a175c002743
Andrey Klimin <andrey7klimin@yandex.ru>
2025-08-23 15:03:28 +0500

    ввод через двойные указатели

--- main.c
+++ main.c
@@ -7,8 +7,8 @@ const double EPS = 1e-6;
 const int MAX_INPUT = 100;
 
 int compareDoubles(const double number1, const double number2);
-int ReadInt(const char str[], int* const i, double* const num);
-int GetCoef(double* const a, double* const b, double* const c);
+int ReadInt(char** input, double* const num);
+int GetCoefs(double* const a, double* const b, double* const c);
 int Solve(const double a, const double b, const double c,
                     double* const root1, double* const root2);
 int SolveLinear(const double a, const double b,
@@ -27,47 +27,60 @@ int compareDoubles(const double number1, const double number2) {
     }
 }
 
-int ReadInt(const char input[], int* const i, double* const num) {
+int ReadInt(char** input, double* const num) {
+    assert(input != NULL);
     assert(num != NULL);
+
     *num = 0;
-    if (input[*i] < '0' || input[*i] > '9') {
+    int sign = 1;
+    if (**input == '-') {
+        sign = -1;
+        (*input)++;
+    }
+    if (**input < '0' || **input > '9') {
         return -1;
     }
-    for (; input[*i] >= '0' && input[*i] <= '9'; *i = *i + 1) {
-        *num = *num * 10 + (input[*i] - '0');
+    for (; **input >= '0' && **input <= '9';(*input)++) {
+        *num = *num * 10 + (**input - '0');
     }
-    if (input[*i] == ' ') {
-        *i = *i + 1;
+    *num = *num * sign;
+    if (**input == ' ') {
+        (*input)++;
         return 0;
     }
-    if (input[*i] == '\n') {
+    if (**input == '\n') {
         return 1;
     }
     return -1;
 }
 
-int GetCoef(double* const a, double* const b, double* const c) {
-    char input[MAX_INPUT];
-
+int GetCoefs(double* const a, double* const b, double* const c) {
+    char str[MAX_INPUT]; // char *input;
+    char *input = str;
     fgets(input, MAX_INPUT, stdin);
-    int i = 0;
-    int status = ReadInt(input, &i, a);
+
+
+    int status = ReadInt(&input, a);
     if (status != 0) {
         return -1;
     }
-    status = ReadInt(input, &i, b);
+
+    status = ReadInt(&input, b);
     if (status != 0) {
         return -1;
     }
-    status = ReadInt(input, &i, c);
+
+    status = ReadInt(&input, c);
+
     if (status != 1) {
         return -1;
     }
+
     return 0;
 }
 
 int Solve(const double a, const double b, const double c,
-                    double* const root1, double* const root2) {
+                  double* const root1, double* const root2) {
     assert(root1 != NULL);
     assert(root2 != NULL);
 
@@ -139,10 +152,11 @@ int main() {
     double a = 0, b = 0, c = 0;
 
 
-    int status = GetCoef(&a, &b, &c);
+    int status = GetCoefs(&a, &b, &c);
+
     while (status != 0) {
         printf("Неправильный формат, укажите 3 числа через пробел:\n");
-        status = GetCoef(&a, &b, &c);
+        status = GetCoefs(&a, &b, &c);
     }
 
     double root1 = 0, root2 = 0;
